236d48227604cd8832020a9f3a7481f138d5dbb6
unknown <2003.nhatcuong@gmail.com>
2024-12-07 05:18:06 +0700

    NhatCuong; Usecase: Xem Lich Su kham Benh

--- mvc/models/mBacsi.php
+++ mvc/models/mBacsi.php
@@ -45,7 +45,7 @@ class MBacsi extends DB
         return json_encode($mang);
     }
 
-    //NhatCuong; Usecase: Xem danh sách khám bệnh; Hàm truy vấn for input-radio:Sáng
+    //NhatCuong; Usecase: Xem danh sách khám bệnh1/3; Hàm truy vấn for input-radio:Sáng 
     public function GetDanhSachKhamSang()
     {
         $str = 'SELECT 
@@ -72,7 +72,7 @@ class MBacsi extends DB
         return json_encode($mang);
     }
 
-    //NhatCuong; Usecase: Xem danh sách khám bệnh; Hàm truy vấn for input-radio:Chiều
+    //NhatCuong; Usecase: Xem danh sách khám bệnh2/3; Hàm truy vấn for input-radio:Chiều
     public function GetDanhSachKhamChieu()
     {
         $str = 'SELECT 
@@ -99,7 +99,7 @@ class MBacsi extends DB
         return json_encode($mang);
     }
 
-    //NhatCuong; Usecase: Xem danh sách khám bệnh; Hàm truy vấn for input-radio:Tất cả
+    //NhatCuong; Usecase: Xem danh sách khám bệnh3/3; Hàm truy vấn for input-radio:Tất cả
     public function GetDanhSachKhamAll()
     {
         $str = 'SELECT 
@@ -124,4 +124,54 @@ class MBacsi extends DB
         }
         return json_encode($mang);
     }
+    //NhatCuong; Usecase 1/3: Xem lịch sử khám bệnh, phiếu khám
+    public function GetPhieuKhamBenhNhan($maBN)
+    {
+        $str = "SELECT 
+                pk2.NgayTao,
+                nv.HoVaTenNV AS BacSi,
+                pk2.TrieuChung,
+                pk2.ChuanDoan,
+                pk2.KetQua,
+                pk2.LoiDan,
+                pk2.NgayTaiKham
+            FROM 
+                PhieuKham2 pk2
+            JOIN 
+                NhanVien nv ON pk2.MaNV = nv.MaNV
+            WHERE 
+                pk2.MaBN = '$maBN'
+            ORDER BY 
+                pk2.NgayTao";
+        $result = mysqli_query($this->con, $str);
+        $mang = array();
+        while ($row = mysqli_fetch_array($result))
+        {
+            $mang[] = $row;
+        }
+        return json_encode($mang);
+    }
+
+    //NhatCuong; Usecase 2/3: Xem lịch sử khám bệnh, thông tin bệnh nhân
+    public function GetThongTinBenhNhan($maBN)
+    {
+        $str = "SELECT MaBN, HovaTen, NgaySinh, GioiTinh, BHYT, DiaChi, SoDT
+            FROM benhnhan
+            WHERE MaBN = '$maBN'";
+        $result = mysqli_query($this->con, $str);
+        $row = mysqli_fetch_assoc($result);
+        return json_encode($row);
+    }
+
+    //NhatCuong; Usecase 2/3: Xem lịch sử khám bệnh //count (phieukham)
+    public function GetSoLanKhamBenh($maBN)
+    {
+        $str = "SELECT COUNT(*) as SoLanKham
+            FROM PhieuKham2
+            WHERE MaBN = '$maBN'";
+        $result = mysqli_query($this->con, $str);
+        $row = mysqli_fetch_assoc($result);
+        return $row['SoLanKham'];
+    }
 }
+
